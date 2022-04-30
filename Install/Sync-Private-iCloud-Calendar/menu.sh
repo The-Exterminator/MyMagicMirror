3cad7e3e717e84810c0e8c6c71738cc057470eb3
The-Exterminator <forum@the-exterminator.dk>
2022-04-30 10:01:53 +0200

    Update menu.sh

--- Install/Sync-Private-iCloud-Calendar/menu.sh
+++ Install/Sync-Private-iCloud-Calendar/menu.sh
@@ -5,7 +5,6 @@ function ask_user() {
 echo -e "
  1.) Sync 1 private iCloud Calendar
  2.) Sync 2 private iCloud Calendar
- 3.) Quit
 \n"
 
 read -e -p "Select 1-3: " choice
@@ -20,13 +19,9 @@ elif [ "$choice" == "2" ]; then
     chmod +x Sync-2-private-iCloud-Calendar.sh
     ./Sync-2-private-iCloud-Calendar.sh
 
-elif [ "$choice" == "3" ]; then
-
-    clear && exit 0
-
 else
 
-    echo "Please select 1, 2, or 3." && sleep 3
+    echo "Please select 1 or 2." && sleep 2
     clear && ask_user
 
 fi
